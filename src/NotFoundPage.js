1b89738f51ff6d8bcba91233232410521493c37a
Vijay Dhyani <vijay97dhyani@gmail.com>
2024-04-21 15:48:53 +0000

    ui change

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>PAGE NOT FOUND!!</div>;
+  return <div className="text-center mt-5 text-bold">PAGE NOT FOUND!!</div>;
 };
 
 export default NotFoundPage;
